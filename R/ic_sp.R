af2baa84ca4c5c4e167b623e76379004a3b924dc
Clifford Anderson-Bergman <pistacliffcho@gmail.com>
2019-05-29 05:00:03 +0000

    version 2.0.10

--- R/ic_sp.R
+++ R/ic_sp.R
@@ -102,7 +102,8 @@ ic_sp <- function(formula, data, model = 'ph', weights = NULL, bs_samples = 0, u
   mt <- callInfo$mt
   
   y <- model.response(mf, "numeric")
-  x <- model.matrix(mt, mf, contrasts)
+#  x <- model.matrix(mt, mf, contrasts)
+  x <- model.matrix(mt, mf)
   if(is.matrix(x))	xNames <- colnames(x)
   else				      xNames <- as.character(formula[[3]])
   if('(Intercept)' %in% colnames(x)){	
